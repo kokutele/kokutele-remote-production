9f89e9edc7ac550dd178edfe04c42683b4e32af4
kensaku komatsu <kensaku.komatsu@gmail.com>
2022-04-20 23:50:00 +0900

    updated home page

--- webapp/src/layouts/home.js
+++ webapp/src/layouts/home.js
@@ -6,12 +6,18 @@ export default function Home(props) {
     <div className="Home" style={{textAlign: "center"}}>
       <h1 style={{
         color:'#CEAC5C',
-        padding: "1em 0 1.5em 0",
+        padding: "1em 0 .5em 0",
         margin: 0,
         fontSize: '72pt',
         fontWeight: 'normal',
         fontFamily: "'Lobster', cursive",
       }}>Kokutele Studio</h1>
+      <h3
+        style={{
+          fontSize: "24pt",
+          padding: "0.2em 0 1.5em 0",
+        }}
+      >Open Source Virtual Studio</h3>
  
       <nav>
         <Button type="primary">
